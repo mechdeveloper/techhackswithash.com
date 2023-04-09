555d231cd418d1d61e5c9edd702434830ff6f013
Ashish Singh Baghel <ashish.sbaghel@outlook.com>
2023-04-09 12:45:39 +0000

    Fix key error for card componentts

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -34,7 +34,7 @@ const Layout = ({ children, pageMeta }) => {
       
       <div className='min-h-screen flex flex-col'>
         <Header />
-          <main className='flex-grow container mx-auto px-4 sm:px-6'>{children}</main>
+        <main className='flex-grow container mx-auto px-4 sm:px-6'>{children}</main>
         <Footer />
       </div>
     </>
